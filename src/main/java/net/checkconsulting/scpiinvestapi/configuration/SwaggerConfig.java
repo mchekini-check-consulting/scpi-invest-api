0fd5f84cc7febd3b4972f680778caa8eea3e5fd6
Moh-Tacine <mytacine.dev@gmail.com>
2024-10-02 18:31:36 +0200

    Add Swagger configuration

---
+++ src/main/java/net/checkconsulting/scpiinvestapi/configuration/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package net.checkconsulting.scpiinvestapi.configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info()
+                        .title("SCPI Invest Plus")
+                        .description("Documentation des APIs de SCPI Invest Plus")
+                        .version("1.0.0"));
+    }
+}
