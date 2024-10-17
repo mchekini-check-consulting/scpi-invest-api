882ab4604ebd2ca93839b2aa30185ff55f978acd
mahdi chekini <mchekini@checkconsulting.fr>
2024-10-17 04:19:21 +0200

    Add APM server

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 
 java -javaagent:/apm.jar \
           -Delastic.apm.service_name=scpi-invest \
-          -Delastic.apm.server_urls=apm-server.logging:8200 \
+          -Delastic.apm.server_urls=http://apm-server.logging:8200 \
           -Delastic.apm.secret_token= \
           -Delastic.apm.application_packages=net.checkconsulting \
           -jar /opt/scpi-invest.jar
