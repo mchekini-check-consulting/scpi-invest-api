96a005ddce347581fcc881910a01ae69c0c236ac
mahdi chekini <mchekini@checkconsulting.fr>
2024-10-17 04:08:54 +0200

    Add APM server

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
 FROM ubuntu
 RUN apt-get update && apt-get install openjdk-17-jdk vim curl -y
+ADD elastic-apm-agent-1.46.0.jar /apm.jar
 WORKDIR /opt
 ADD target/scpi-invest-*.jar scpi-invest.jar
+ADD script.sh /script.sh
 EXPOSE 8080
-ENTRYPOINT ["java", "-jar", "/opt/scpi-invest.jar"]
+ENTRYPOINT ["/script.sh"]
---
+++ script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+java -javaagent:/apm.jar \
+          -Delastic.apm.service_name=scpi-invest \
+          -Delastic.apm.server_urls=apm-server.logging:8200 \
+          -Delastic.apm.secret_token= \
+          -Delastic.apm.application_packages=net.checkconsulting \
+          -jar /opt/scpi-invest.jar
