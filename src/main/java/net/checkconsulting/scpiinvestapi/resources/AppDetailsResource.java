0cecb35fb446e74fd22a464f81ed97fc8ca03729
mahdi chekini <mchekini@checkconsulting.fr>
2024-10-17 02:38:34 +0200

    Add Application Name and version logging

--- src/main/java/net/checkconsulting/scpiinvestapi/resources/AppDetailsResource.java
+++ src/main/java/net/checkconsulting/scpiinvestapi/resources/AppDetailsResource.java
@@ -1,6 +1,7 @@
 package net.checkconsulting.scpiinvestapi.resources;
 
 import io.swagger.v3.oas.annotations.Operation;
+import lombok.extern.slf4j.Slf4j;
 import net.checkconsulting.scpiinvestapi.dto.AppDetailsDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1")
+@Slf4j
 public class AppDetailsResource {
 
 
@@ -15,6 +17,8 @@ public class AppDetailsResource {
             description = "Renvoie le nom de l'application ainsi que la version actuelle")
     @GetMapping("details")
     public AppDetailsDto getApplicationDetails(){
+
+        log.info("Application name = SCPI-INVEST+ , version = 1.0.0");
 
         return AppDetailsDto.builder()
                 .applicationName("SCPI-INVEST+")
